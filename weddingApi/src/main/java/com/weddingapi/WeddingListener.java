894a39e6fead8bbc85fb2577ff5579a05a88b8a4
Neel Jain <neeljain198@gmail.com>
2016-10-01 23:04:05 -0400

    add header to rest responsE

--- weddingApi/src/main/java/com/weddingapi/WeddingListener.java
+++ weddingApi/src/main/java/com/weddingapi/WeddingListener.java
@@ -2,16 +2,22 @@ package com.weddingapi;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
 
 @Path("/weddinginfo")
 public class WeddingListener {
 	@GET
 	@Produces({MediaType.APPLICATION_JSON})
-	public Wedding getInfo() {
-		Wedding w1 = new Wedding("WED1", "ABC", "XYZ", "204 10th Street", "");
-		return w1;
+	public Response getInfo() {
+		Wedding w1 = new Wedding("WED1", "ABC", "XYwerwZ", "204 10th Street", "");
+		System.out.println("Recieved it mahn");
+		return Response.status(200)
+				.header("Access-Control-Allow-Origin", "*")
+				.entity(w1).build();
+			
 	}
 }
