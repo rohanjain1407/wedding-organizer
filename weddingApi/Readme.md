e3ae808518b70fee255117fcf3019e3cba475b95
Rohan Jain <rohanjain.1407@gmail.com>
2017-02-04 12:59:06 -0500

    WED-8 : Code review changes

--- weddingApi/Readme.md
+++ weddingApi/Readme.md
@@ -5,3 +5,24 @@
   4. Make the rest api call  
     `http://localhost:8080/weddingApi/wedding/weddinginfo`  
     _*assuming server is running on port 8080_
+
+
+Wedding API
+
+Wedding
+Get (GET)
+http://localhost:8080/weddingApi/rest/wedding/get/1
+
+Query (GET)
+http://localhost:8080/weddingApi/rest/wedding/query
+
+Device Token
+Add (POST)
+http://localhost:8080/weddingApi/rest/deviceToken/add
+BODY -
+{
+    "device_token": "token_here",
+    "weddingId": "1"
+}
+
+
--- weddingApi/src/main/java/com/weddingapi/services/WeddingResource.java
+++ weddingApi/src/main/java/com/weddingapi/services/WeddingResource.java
@@ -1,4 +1,4 @@
-package com.weddingapi.getAPI;
+package com.weddingapi.services;
 
 import com.weddingapi.util.HibernateUtil;
 import com.weddingapi.db.Wedding;
@@ -10,19 +10,21 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.List;
 
 /**
- * @author Rohan Jain
+ * @author Neel Jain
  */
-@Path("/weddinginfo")
-public class WeddingGet {
+@Path("/wedding")
+public class WeddingResource {
 
     @GET
-    @Path("/getWedding/{id}")
+    @Path("/get/{id}")
     @Produces({MediaType.APPLICATION_JSON})
-    public Response getWeddingById(@PathParam("id") String weddingId) {
+    public Response get(@PathParam("id") String weddingId) {
         Session session = HibernateUtil.getSessionFactory().openSession();
         Transaction tx = session.beginTransaction();
         try {
@@ -31,6 +33,26 @@ public class WeddingGet {
             tx.commit();
             wed = wed != null ? wed : new Wedding();
             return Response.status(200).entity(wed).header("Access-Control-Allow-Origin", "*").build();
+        }
+        catch (Exception ex) {
+            throw ex;
+        }
+        finally {
+            session.close();
+        }
+    }
+
+    @GET
+    @Path("/query")
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response query() {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = session.beginTransaction();
+        try {
+            List<Wedding> weddingList = session.createCriteria(Wedding.class).list();
+            tx.commit();
+            GenericEntity<List<Wedding>> entity = new GenericEntity<List<Wedding>>(weddingList){};
+            return Response.status(200).entity(entity).header("Access-Control-Allow-Origin", "*").build();
         }
         catch (Exception ex) {
             throw ex;
